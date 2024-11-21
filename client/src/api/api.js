729d8dada830b922cd25bc15efb5e58b55be7627
MikiZenebe <mikizenebe10gmail.com>
2024-11-21 20:12:27 +0300

    updated

--- client/src/api/api.js
+++ client/src/api/api.js
@@ -1,8 +1,8 @@
 import axios from "axios";
 const local = "https://multivendor-server-z8kg.onrender.com";
-const production = "";
+const production = "https://multivendor-server-z8kg.onrender.com";
 const api = axios.create({
-  baseURL: `${local}/api`,
+  baseURL: `${production}/api`,
   withCredentials: true,
 });
 export default api;
